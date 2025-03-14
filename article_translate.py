47dff8b1d78e1fbe0534c0a5dd20faa3a15adc58
Menooker <myjisgreat@live.cn>
2025-03-14 11:41:36 +0800

    ocr and translate

---
+++ article_translate.py
@@ -0,0 +1,95 @@
+import time
+import os
+import google.generativeai as genai
+from tqdm import tqdm
+import argparse
+
+def get_model(key: str, hint: str):
+    # generation_config = {
+    #     "temperature": 1,
+    #     "top_p": 0.95,
+    #     "top_k": 0,
+    #     "max_output_tokens": 1048576,
+    # }
+
+    safety_settings = [
+        {
+            "category": "HARM_CATEGORY_HARASSMENT",
+            "threshold": "BLOCK_NONE"
+        },
+        {
+            "category": "HARM_CATEGORY_HATE_SPEECH",
+            "threshold": "BLOCK_NONE"
+        },
+        {
+            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+            "threshold": "BLOCK_NONE"
+        },
+        {
+            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+            "threshold": "BLOCK_NONE"
+        },
+    ]
+
+    system_instruction = '''翻译这篇文章。只需要翻译内容本身。如果你认为有一些需要补充的译注可以在文中通过括号（）插入，例如汉语语境当中较难翻译的地方。'''
+    if hint:
+        system_instruction += f"\n上下文: [{hint}]"
+    genai.configure(api_key=key,  transport="rest")
+    model = genai.GenerativeModel(model_name="gemini-2.0-flash",
+                                  system_instruction=system_instruction,
+                                  safety_settings=safety_settings)
+    return model
+
+       
+
+def translate(model, base: str, resume: int, outfile):
+    with open(base, encoding="utf-8-sig") as f:
+        lines = f.readlines()
+    bar = tqdm(total=len(lines), initial=resume)
+    idx = resume
+    batch = 50
+    prompt_parts = []
+    while idx < len(lines):
+        batch_data = lines[idx: idx+batch]
+        prompt_parts.append(
+            {"role":"user", "parts":"".join(batch_data)})
+        for retries in range(6):
+            try:
+                response = model.generate_content(prompt_parts, request_options={"timeout": 600})
+                cleaned = response.text
+                outfile.write(cleaned)
+                if cleaned[-1] != "\n":
+                    outfile.write("\n")
+                outfile.flush()
+                time.sleep(3)
+                break
+            except Exception as e:
+                if retries == 5:
+                    raise e
+                print(f"Error!!!!!!!!!!!!!!{e}\\nsleeping")
+                if " An internal error has occurred." in str(e):
+                    time.sleep(20)
+                else:
+                    time.sleep(60)
+        bar.update(len(batch_data))
+        idx += len(batch_data)
+        prompt_parts.append(response.candidates[0].content)
+        prompt_parts = prompt_parts[-4:]
+    # image.save(os.path.join(base, new_fn) + ".jpg", optimize=True)
+
+def main(key: str, base: str, out: str, resume: int, hint: str):
+    model = get_model(key, hint)
+    with open(out, 'a', encoding="utf-8-sig") as f:
+        translate(model, base, resume, f)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--key", type=str, required=True)
+    parser.add_argument("--base", type=str, required=True)
+    parser.add_argument("--out", type=str, required=True)
+    parser.add_argument("--hint", type=str, default="")
+    parser.add_argument('--resume', type=int, default=0)
+
+    args = parser.parse_args()
+    main(args.key, args.base, args.out, args.resume, args.hint)
---
+++ ocr.py
@@ -0,0 +1,110 @@
+import time
+from PIL import Image
+import os
+from pathlib import Path
+import google.generativeai as genai
+from tqdm import tqdm
+import argparse
+
+def get_model(key: str):
+    # generation_config = {
+    #     "temperature": 1,
+    #     "top_p": 0.95,
+    #     "top_k": 0,
+    #     "max_output_tokens": 1048576,
+    # }
+
+    safety_settings = [
+        {
+            "category": "HARM_CATEGORY_HARASSMENT",
+            "threshold": "BLOCK_NONE"
+        },
+        {
+            "category": "HARM_CATEGORY_HATE_SPEECH",
+            "threshold": "BLOCK_NONE"
+        },
+        {
+            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+            "threshold": "BLOCK_NONE"
+        },
+        {
+            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+            "threshold": "BLOCK_NONE"
+        },
+    ]
+
+    system_instruction = '''Output the contents of this article. Only the contents texts are needed.
+Before outputing, remove the line breaks at the edge of the book, or in the middle of a sentence. Remove them.
+Ignore the book page number.'''
+    genai.configure(api_key=key,  transport="rest")
+    model = genai.GenerativeModel(model_name="gemini-2.0-flash",
+                                  system_instruction=system_instruction,
+                                  safety_settings=safety_settings)
+    return model
+
+allowed_end = {'」', '。', '>', '，', '、'}
+def clean_line_breaks(s: str) -> str:
+    spl = s.split("\n")
+    newlines = []
+    for idx, line in enumerate(spl):
+        strp = line.strip()
+        if strp == "" or strp[-1] in allowed_end:
+            newlines.append(line)
+            newlines.append("\n")
+            continue
+        newlines.append(line[:-1])
+    return "".join(newlines)
+        
+
+        
+
+def do_ocr(model, base: str, resume: int, outfile):
+    files = [f for f in os.listdir(base) if f.lower().endswith(".jpg") and not f.startswith("compressed_") and os.path.isfile(os.path.join(base, f))]
+    files = sorted(files)
+    bar = tqdm(total=len(files), initial=resume)
+    for idx, f in enumerate(files):
+        if idx < resume:
+            continue
+        src = os.path.join(base, f)
+        image = Image.open(src)
+        # x,y = image.size
+        # image = image.rotate(180, Image.NEAREST, expand = 1)
+        # new_fn = "compressed_" + Path(f).stem
+        # print(src, "->", new_fn)
+        image = image.resize((1024, 768), Image.Resampling.LANCZOS)
+        prompt_parts=[image]
+        for retries in range(6):
+            try:
+                response = model.generate_content(prompt_parts, request_options={"timeout": 600})
+                cleaned = clean_line_breaks(response.text)
+                outfile.write(cleaned)
+                outfile.write("\n\n")
+                outfile.flush()
+                time.sleep(3)
+                break
+            except Exception as e:
+                if retries == 5:
+                    raise e
+                print(f"Error!!!!!!!!!!!!!!{e}\\nsleeping")
+                if " An internal error has occurred." in str(e):
+                    time.sleep(20)
+                else:
+                    time.sleep(60)
+        bar.update(1)
+    # image.save(os.path.join(base, new_fn) + ".jpg", optimize=True)
+
+def main(key: str, base: str, out: str, resume: int):
+    model = get_model(key)
+    with open(out, 'a', encoding="utf-8-sig") as f:
+        do_ocr(model, base, resume, f)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--key", type=str, required=True)
+    parser.add_argument("--base", type=str, required=True)
+    parser.add_argument("--out", type=str, required=True)
+    parser.add_argument('--resume', type=int, default=0)
+
+    args = parser.parse_args()
+    main(args.key, args.base, args.out, args.resume)
