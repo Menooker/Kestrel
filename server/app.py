77e446e850cb8392e5bb25bd517609afb0b803b3
Menooker <myjisgreat@live.cn>
2025-11-01 20:55:30 +0800

    add

--- server/app.py
+++ server/app.py
@@ -93,4 +93,4 @@ def progress():
     return jsonify({'output': content, 'running': running, 'step': step})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
